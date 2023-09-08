7aab381f0dd00a77b4037455bc2f25cbe7e4d558
mivan08 <mykhailoivanov662@gmail.com>
2023-09-08 15:37:48 +0200

    url fixed

--- firstjob/firstform/urls.py
+++ firstjob/firstform/urls.py
@@ -4,5 +4,5 @@ from .views import SecondFormCreateView
 
 urlpatterns = [
     path('', FirstFormCreateView.as_view(), name='first_form'),
-    path('next=/secondform/createview/model1/<int:id>', SecondFormCreateView.as_view(), name='second_form'),
+    path('?next=/secondform/createview/model1/<int:id>', SecondFormCreateView.as_view(), name='second_form'),
 ]
